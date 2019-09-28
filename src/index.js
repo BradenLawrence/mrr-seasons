5db595e11e240b800679e3aedeca15d8008f058d
blip <b@bradenlawrence.com>
2019-09-27 20:05:14 -0400

    add error message handling to state

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      lat: null
+      lat: null,
+      errMsg: ''
     }
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState( {lat: position.coords.latitude} ),
-      (err) => console.log(err)
+      err => this.setState( {errMsg: err.message} )
     )
   }
   
   render() {
     return(
-      <h1>Latitude: { this.state.lat }</h1>
+      <div>
+        <h1>Latitude: { this.state.lat }</h1>
+        <aside>{ this.state.errMsg }</aside>
+      </div>
     )
   }
 }
