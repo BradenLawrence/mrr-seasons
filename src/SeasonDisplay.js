9551f26b88103e7021d2be9fe5f6c760d59244d5
blip <b@bradenlawrence.com>
2019-10-01 15:13:56 -0400

    move season logic to SeasonDisplay and add display text

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const getSeason = (lat, month) => {
+  if (month>2 && month<9) {
+    return lat > 0 ? 'summer':'winter'
+  } else {
+    return lat > 0 ? 'winter':'summer'
+  }
+}
+
 const SeasonDisplay = (props) => {
+  let season = getSeason(props.lat, new Date().getMonth())
+  let seasonText = season === 'winter' ? "Brr, it's chilly!" : "Let's hit the beach!"
   return(
-    <div>Season Display</div>
+    <h1>{ seasonText }</h1>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
   constructor(props) {
@@ -8,17 +9,20 @@ class App extends React.Component {
       lat: null,
       errMsg: ''
     }
+  }
+  
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState( {lat: position.coords.latitude} ),
       err => this.setState( {errMsg: err.message} )
     )
   }
-  
+
   render() {
       if(this.state.errMsg && !this.state.lat) {
         return <div>Error: { this.state.errMsg }</div>
       } else if(this.state.lat && !this.state.errMsg) {
-        return <div>Lat: { this.state.lat }</div>
+        return <SeasonDisplay lat={ this.state.lat } />
       } else {
         return <div>Loading...</div>
       }
